6b70be366c478205cd633989823972bdc469589b
Hugo Dor <hugo.dor@student.kdg.be>
2025-11-21 08:26:49 +0100

    Sidebar weg gedaan

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import {Route, Routes} from "react-router-dom";
 
 import IndexPage from "@/pages/index";
-import DefaultLayout from "@/layouts/default.tsx";
 
 function App() {
     return (
-        <DefaultLayout>
-            <Routes>
-                <Route element={<IndexPage/>} path="/"/>
-            </Routes>
-        </DefaultLayout>
+        <Routes>
+            <Route element={<IndexPage/>} path="/"/>
+        </Routes>
     );
 }
 
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,15 +1,11 @@
+// DefaultLayout.tsx
 import {ReactNode} from "react";
-
-import {SideBar} from "@/components/SIdeBar.tsx";
 
 export default function DefaultLayout({children}: { children: ReactNode }) {
     return (
         <div className="relative w-full h-screen overflow-hidden">
-            {/* Sidebar - fixed position, always on the left */}
-            <SideBar/>
-
-            {/* Main content - full width, padding to account for collapsed sidebar */}
-            <div className="w-full h-full overflow-auto pl-16">
+            <div
+                className={`w-full h-full overflow-auto transition-all duration-300 pl-16`}>
                 <div className="max-w-7xl mx-auto p-4">
                     {children}
                 </div>
