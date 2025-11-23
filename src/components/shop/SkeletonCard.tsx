0c601d069391ad54123de167c0db075d25cb2228
Hugo Dor <hugo.dor@student.kdg.be>
2025-11-23 12:18:29 +0100

    game cards aangepast voor thema

--- src/components/shop/SkeletonCard.tsx
+++ src/components/shop/SkeletonCard.tsx
@@ -3,18 +3,20 @@ import {Skeleton} from "@heroui/skeleton";
 
 export function SkeletonCard() {
     return (
-        <Card className={"py-4 w-[250px] h-[350px] sm:w-[300px]"}>
+        <Card className={"py-4 w-[250px] h-[350px] sm:w-[300px] bg-black/20 backdrop-blur-md border border-white/10"}>
             <CardBody className={"overflow-visible py-2 px-4 flex justify-center items-center"}>
-                <Skeleton className={"object-cover rounded-xl aspect-video w-full h-full"}/>
+                <Skeleton className={"object-cover rounded-xl aspect-video w-full h-full bg-white/10"}>
+                    <div className={"w-full h-full"}></div>
+                </Skeleton>
             </CardBody>
             <CardHeader className={"pt-2 px-4 flex-col items-start"}>
-                <Skeleton className={"w-3/5 rounded-lg"}>
-                    <div className={"h-5 rounded-lg bg-default-200"}></div>
+                <Skeleton className={"w-3/5 rounded-lg bg-white/10"}>
+                    <div className={"h-5 rounded-lg"}></div>
                 </Skeleton>
             </CardHeader>
             <CardFooter className={"pt-0 px-4 pb-4 flex-col items-end overflow-hidden"}>
-                <Skeleton className={"w-1/6 rounded-lg"}>
-                    <div className={"h-4 rounded-lg bg-primary"}></div>
+                <Skeleton className={"w-1/6 rounded-lg bg-white/10"}>
+                    <div className={"h-4 rounded-lg"}></div>
                 </Skeleton>
             </CardFooter>
         </Card>
