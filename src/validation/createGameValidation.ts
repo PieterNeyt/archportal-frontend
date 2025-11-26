85030a6454165083b4855db020bfd8cebfdccdd3
Hugo Dor <hugo.dor@student.kdg.be>
2025-11-26 22:47:19 +0100

    Pushed

--- src/validation/createGameValidation.ts
+++ src/validation/createGameValidation.ts
@@ -2,11 +2,11 @@ import z from "zod"
 import {GameGenre} from "@/model/GameGenre.ts";
 
 export const createGameSchema = z.object({
-    title: z.string(),
-    description: z.string(),
+    title: z.string().max(255),
+    description: z.string().max(255),
     price: z.number().positive(),
-    imageUrl: z.string(),
-    gameUrl: z.string(),
+    imageUrl: z.string().max(255),
+    gameUrl: z.string().max(255),
     genre: z.enum(GameGenre)
 })
 
