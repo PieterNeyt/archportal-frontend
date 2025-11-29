ccb65519ccec76bc1e744cdb8d709bab8258e494
Cian Van Acker <cian.vanacker@student.kdg.be>
2025-11-29 02:18:28 +0100

    fix error

--- src/model/user.ts
+++ src/model/user.ts
@@ -2,7 +2,7 @@ import {Profile} from "@/model/profile.ts";
 
 export interface User extends Profile {
     hasStudio: boolean;
-    studioId: string;
+    studioId: string | undefined;
     name: string | undefined;
     roles: string[];
 }
