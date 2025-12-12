04e038ef901076d73f16fa8e41bef24d30532962
Cian Van Acker <cian.vanacker@student.kdg.be>
2025-12-12 19:07:00 +0100

    authenticating component

--- src/components/security/RouteGuardLoggedIn.tsx
+++ src/components/security/RouteGuardLoggedIn.tsx
@@ -1,5 +1,6 @@
 import {PropsWithChildren, useContext, useEffect} from "react";
 import SecurityContext from "@/context/SecurityContext.ts";
+import Authenticating from "@/components/security/Authenticating.tsx";
 
 export default function RouteGuardLoggedIn({children}: PropsWithChildren) {
     const {isInitialised, isAuthenticated, loggedInUser, login} = useContext(SecurityContext);
@@ -11,7 +12,7 @@ export default function RouteGuardLoggedIn({children}: PropsWithChildren) {
     }, [isAuthenticated, isInitialised, loggedInUser?.hasStudio, login]);
 
     if (!isInitialised || !isAuthenticated()) {
-        return <div>Authenticating</div>;
+        return <Authenticating/>;
     }
 
     return children;
