ec4a4f10df5af5106d904d8bd6638dc6c3227207
Cian Van Acker <cian.vanacker@student.kdg.be>
2025-12-12 10:11:16 +0100

    oplossen code duplication in route guards

--- src/components/security/RouteGuardGameStudio.tsx
+++ src/components/security/RouteGuardGameStudio.tsx
@@ -1,31 +1,26 @@
 import {PropsWithChildren, useContext, useEffect} from "react";
 import SecurityContext from "@/context/SecurityContext.ts";
 import {useNavigate} from "react-router-dom";
+import RouteGuardLoggedIn from "@/components/security/RouteGuardLoggedIn.tsx";
 
 export default function RouteGuardGameStudio({children}: PropsWithChildren) {
-    const {isInitialised, isAuthenticated, loggedInUser, login} = useContext(SecurityContext);
+    const {loggedInUser} = useContext(SecurityContext);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (isInitialised && !isAuthenticated()) {
-            login();
-        }
-    }, [isAuthenticated, isInitialised, loggedInUser?.hasStudio, login]);
-
-    useEffect(() => {
         if (loggedInUser?.hasStudio) {
-            if (window.location.pathname !== "/gamestudio"){
+            if (window.location.pathname !== "/gamestudio") {
                 navigate("/gamestudio");
             }
-        }else {
+        } else {
             if (window.location.pathname !== "/create/gamestudio")
                 navigate("/create/gamestudio")
         }
     }, [loggedInUser?.hasStudio, navigate]);
 
-    if (!isInitialised || !isAuthenticated()) {
-        return <div>Authenticating</div>;
-    }
-
-    return children;
+    return (
+        <RouteGuardLoggedIn>
+            {children}
+        </RouteGuardLoggedIn>
+    );
 }
