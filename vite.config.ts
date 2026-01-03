c3747830a563a3d394a43e4544d2d558bcf81e88
axel peeter <axel.peeters.1@student.kdg.be>
2026-01-03 21:45:19 +0100

    backend moved to 8090 in dev so following here

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://0.0.0.0:8080"
+                target: "http://0.0.0.0:8090"
             }
         }
     }
